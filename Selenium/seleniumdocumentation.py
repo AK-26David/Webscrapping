0d8ad875e749840f9f9735a10d5c717db3a9a8e1
Arnav <arnav.karnik@gmail.com>
2024-06-07 15:50:00 +0530

    test

--- Selenium/seleniumdocumentation.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-import chromedriver_autoinstaller
- 
- 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-                                      # and if it doesn't exist, download it automatically,
