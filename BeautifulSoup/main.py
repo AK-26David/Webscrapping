d42c4f0cd0f3c0d40658b5542e0694b63f3fc647
AK-26David <arnav.karnik@gmail.com>
2024-06-17 12:41:21 +0530

    Bug fix

--- BeautifulSoup/main.py
+++ BeautifulSoup/main.py
@@ -1,4 +1,10 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup  # Import BeautifulSoup class from bs4 module
+
+# Open the HTML file named 'home.html' in read mode
 with open('home.html', 'r') as html_file:
-    content = html_file.read()
-    soup = BeautifulSoup(content, 'html.parser')
+    content = html_file.read()  # Read the entire content of the file into 'content'
+
+# Create a BeautifulSoup object from the HTML content using the built-in HTML parser
+soup = BeautifulSoup(content, 'html.parser')
+
+# Now you can use the 'soup' object to interact with the parsed HTML
